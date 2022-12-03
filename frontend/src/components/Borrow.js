67a3692b2767dfcadc52acfe1ab351222693424d
elitex45 <gurugkgurukiran@gmail.com>
2022-12-04 00:04:20 +0530

    added one more table

--- frontend/src/components/Borrow.js
+++ frontend/src/components/Borrow.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Space, Table, Tag, Button, Input } from 'antd';
 import { useSelector } from 'react-redux';
 import BorrowModal from './Modals/BorrowModal';
+import RepayModal from './Modals/RepayModal';
 
 
 export default function Borrow(props) {
@@ -55,6 +56,45 @@ export default function Borrow(props) {
   })
     : ""
 
+
+
+  const columnsBorrowed = [
+    {
+      title: 'Asset', //props.titles.asset
+      dataIndex: 'asset',
+      key: 'asset',
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: 'Annual Percentage Yield',
+      dataIndex: 'apy',
+      key: 'apy',
+    },
+    {
+      title: '',
+      dataIndex: 'Debt',
+      key: 'Debt',
+      render: (_, record) => (
+        <Space size='middle'>
+          <RepayModal/>
+        </Space>
+      )
+    },
+  ];
+
+  const reservesListBorrowed = []
+  const dataBorrowed = reserves ? reserves.map((data, key) => {
+    console.log("data: ", data.name)
+    reservesList.push(
+      {
+        key: key,
+        asset: data.name,
+        apy: "10%",
+      })
+  })
+    : ""
+
+
   return (
     <div style={{ height: '82.5vh' }}>
       <h1 style={{ textAlign: 'center' }}>
@@ -62,6 +102,8 @@ export default function Borrow(props) {
       </h1>
       <h2 style={{ textAlign: 'center' }}>Borrow Asset Table</h2>
       <Table columns={columns} dataSource={reservesList} />
+      <h2>Borrowed assets</h2>
+      <Table columns={columnsBorrowed} dataSource={reservesListBorrowed} />
     </div>
   );
 }
