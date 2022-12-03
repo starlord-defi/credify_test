771e3b099c8d9e307fb13ba51f63cf1d7ea396af
elitex45 <gurugkgurukiran@gmail.com>
2022-12-03 23:05:39 +0530

    working

--- contracts/CredifyPremiumNFT.sol
+++ contracts/CredifyPremiumNFT.sol
@@ -1,37 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
 
-import './dependencies/openzeppelin/contracts721/token/ERC721/ERC721.sol';
-import './dependencies/openzeppelin/contracts721/token/ERC721/ERC721Burnable.sol';
-import './dependencies/openzeppelin/contracts721/access/Ownable.sol';
-import './dependencies/openzeppelin/contracts721/utils/Counters.sol';
+import "./dependencies/openzeppelin/contracts721/token/ERC721/ERC721.sol";
+import "./dependencies/openzeppelin/contracts721/token/ERC721/ERC721Burnable.sol";
+import "./dependencies/openzeppelin/contracts721/access/Ownable.sol";
+import "./dependencies/openzeppelin/contracts721/utils/Counters.sol";
 
-contract CredifyPremiumNFT is ERC721, ERC721Burnable, Ownable {
+import "hardhat/console.sol";
+contract CredifyPremiumNFT is ERC721("CredifyPremiumNFT", "CP"), ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-
-    constructor() ERC721("CredifyPremiumNFT", "CP") public {}
 
     function _baseURI() internal pure returns (string memory) {
         return "ipfs://";
     }
 
-    function mintCredifyPremium(string memory _tokenURI,address to) public payable {
-        require(to!=address(0),"cannot mint to zero address");
-
-        //check if the balance of credit score in tokens
-        //if more than 50 then mint the nft if not revert
-
-
-        uint256 tokenId = _tokenIdCounter.current();
+    function mintCredifyPremium(uint256 score,address to) public {
+        require(to != address(0), "cannot mint to zero address");
+        require(score >= 50 , "credit score must be more than 50");
+        require(super.balanceOf(to)<=1,"cannot have more than one nft badge");
         _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, _tokenURI);
     }
 
-    function burnCredifyPremium(uint256 tokenId)public {
-        require(msg.sender == ownerOf(tokenId),"only owner can burn");
+    function burnCredifyPremium(uint256 tokenId) public {
+        require(msg.sender == super.ownerOf(tokenId), "only owner can burn");
         _burn(tokenId);
     }
 }
